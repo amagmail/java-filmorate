216aab8cb72f60bf5790a783f6cf22f98b8d5b20
amagmail <skynet@skynet.ru>
2025-03-05 09:02:47 +0300

    Убрал лишние проверки

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -18,11 +18,8 @@ public class Film {
     private Long id;
     private Set<Long> likes = new HashSet<>();
     private Set<Genre> genres = new HashSet<>();
-    private Mpa mpa;
-
-    @NotNull
-    @NotEmpty
     private Set<Director> directors = new HashSet<>();
+    private Mpa mpa;
 
     @NotNull
     @NotBlank
